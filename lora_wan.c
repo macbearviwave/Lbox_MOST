c48987a6a0cc1939fda694f00dc185b52651b4de
Macbear Chen <macbear.chen@viwave.com>
2017-03-15 16:55:41 +0800

    refactor: lora_wan AT command

--- lora_wan.c
+++ lora_wan.c
@@ -80,7 +80,7 @@ void WANrestore()
 
 /////////////////////////////////////////
 // send payload by Tx: port: 1~223, cnf/uncnf, Hex-payload(11 bytes)
-boolean MOSTLoraWAN::setTx(int port, char *confirm, char *payload)
+boolean WANsetTx(int port, char *confirm, char *payload)
 {
     sprintf(_strBuf, "AAT2 Tx=%d,%s,%s", port, confirm, payload);
     char *strRet = command(_strBuf);
@@ -90,33 +90,33 @@ boolean MOSTLoraWAN::setTx(int port, char *confirm, char *payload)
 
 /////////////////////////////////////////
 // TestMode: 0 - disable, 1 - enable
-boolean MOSTLoraWAN::setTestMode(int mode)
+boolean WANsetTestMode(int mode)
 {
     sprintf(_strBuf, "AAT1 TestMode=%d", mode);
     command(_strBuf);
 }
-int MOSTLoraWAN::getTestMode()
+int WANgetTestMode()
 {
     char *strRet = command(F("AAT1 TestMode=?"));
     return atoi(strRet);
 }
 /////////////////////////////////////////
 // DevAddr 02410113
-boolean MOSTLoraWAN::setDevAddr(const char *strAddr)
+boolean WANsetDevAddr(const char *strAddr)
 {
     sprintf(_strBuf, "AAT2 DevAddr=%s", strAddr);
     char *strRet = command(_strBuf);
 
     return isOK(strRet);
 }
-char *MOSTLoraWAN::getDevAddr()
+const char *WANgetDevAddr()
 {
     return command(F("AAT2 DevAddr=?"));
 }
 
 /////////////////////////////////////////
 // DevEui
-boolean MOSTLoraWAN::setDevEui(const char *strEui)
+boolean WANsetDevEui(const char *strEui)
 {
     sprintf(_strBuf, "AAT2 DevEui=%s", strEui);
     char *strRet = command(_strBuf);
@@ -128,73 +128,73 @@ const char *WANgetDevEui()
 {
     return command("AAT2 DevEui=?");
 }
-/*
+
 /////////////////////////////////////////
 // AppEui
-boolean MOSTLoraWAN::setAppEui(const char *strEui)
+boolean WANsetAppEui(const char *strEui)
 {
     sprintf(_strBuf, "AAT2 AppEui=%s", strEui);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-char *MOSTLoraWAN::getAppEui()
+const char *WANgetAppEui()
 {
     return command(F("AAT2 AppEui=?"));
 }
 
 /////////////////////////////////////////
 // NwkSKey
-boolean MOSTLoraWAN::setNwkSKey(const char *strKey)
+boolean WANsetNwkSKey(const char *strKey)
 {
     sprintf(_strBuf, "AAT2 NwkSKey=%s", strKey);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-char *MOSTLoraWAN::getNwkSKey()
+const char *WANgetNwkSKey()
 {
     return command(F("AAT2 NwkSKey=?"));
 }
 
 /////////////////////////////////////////
 // AppSKey
-boolean MOSTLoraWAN::setAppSKey(const char *strKey)
+boolean WANsetAppSKey(const char *strKey)
 {
     sprintf(_strBuf, "AAT2 AppSKey=%s", strKey);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-char *MOSTLoraWAN::getAppSKey()
+const char *WANgetAppSKey()
 {
     return command(F("AAT2 AppSKey=?"));
 }
 
 /////////////////////////////////////////
 // AppKey
-boolean MOSTLoraWAN::setAppKey(const char *strKey)
+boolean WANsetAppKey(const char *strKey)
 {
     sprintf(_strBuf, "AAT2 AppKey=%s", strKey);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-char *MOSTLoraWAN::getAppKey()
+const char *WANgetAppKey()
 {
     return command(F("AAT2 AppKey=?"));
 }
 
 /////////////////////////////////////////
 // ADR: 0 - disable, 1 - enable
-boolean MOSTLoraWAN::setADR(int state)
+boolean WANsetADR(int state)
 {
     sprintf(_strBuf, "AAT2 ADR=%d", state);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-int MOSTLoraWAN::getADR()
+int WANgetADR()
 {
     char *strRet = command(F("AAT2 ADR=?"));
     return atoi(strRet);
@@ -202,14 +202,14 @@ int MOSTLoraWAN::getADR()
 
 /////////////////////////////////////////
 // EVK_TxCycle: 1~254 sec
-boolean MOSTLoraWAN::setEVK_TxCycle(int interval)
+boolean WANsetEVK_TxCycle(int interval)
 {
     sprintf(_strBuf, "AAT1 EVK_TxCycle=%d", interval);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-int MOSTLoraWAN::getEVK_TxCycle()
+int WANgetEVK_TxCycle()
 {
     char *strRet = command(F("AAT1 EVK_TxCycle=?"));
     return atoi(strRet);
@@ -217,14 +217,14 @@ int MOSTLoraWAN::getEVK_TxCycle()
 
 /////////////////////////////////////////
 // JoinMode: 0 - disable, 1 - enable
-boolean MOSTLoraWAN::setJoinMode(int mode)
+boolean WANsetJoinMode(int mode)
 {
     sprintf(_strBuf, "AAT2 JoinMode=%d", mode);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-int MOSTLoraWAN::getJoinMode()
+int WANgetJoinMode()
 {
     char *strRet = command(F("AAT2 JoinMode=?"));
     return atoi(strRet);
@@ -232,14 +232,14 @@ int MOSTLoraWAN::getJoinMode()
 
 /////////////////////////////////////////
 // reTx: 0~8
-boolean MOSTLoraWAN::setreTx(int retry)
+boolean WANsetreTx(int retry)
 {
     sprintf(_strBuf, "AAT2 reTx=%d", retry);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-int MOSTLoraWAN::getreTx()
+int WANgetreTx()
 {
     char *strRet = command(F("AAT2 reTx=?"));
     return atoi(strRet);
@@ -247,14 +247,14 @@ int MOSTLoraWAN::getreTx()
 
 /////////////////////////////////////////
 // RxDelay1: 100000~10000000
-boolean MOSTLoraWAN::setRxDelay1(long delayMicroSec)
+boolean WANsetRxDelay1(long delayMicroSec)
 {
     sprintf(_strBuf, "AAT2 RxDelay1=%ld", delayMicroSec);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-long MOSTLoraWAN::getRxDelay1()
+long WANgetRxDelay1()
 {
     char *strRet = command(F("AAT2 RxDelay1=?"));
     return atol(strRet);
@@ -262,14 +262,14 @@ long MOSTLoraWAN::getRxDelay1()
 
 /////////////////////////////////////////
 // DutyCycle: 0 - disable, 1 - enable
-boolean MOSTLoraWAN::setDutyCycle(int state)
+boolean WANsetDutyCycle(int state)
 {
     sprintf(_strBuf, "AAT2 DutyCycle=%d", state);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-int MOSTLoraWAN::getDutyCycle()
+int WANgetDutyCycle()
 {
     char *strRet = command(F("AAT2 DutyCycle=?"));
     return atoi(strRet);
@@ -277,14 +277,14 @@ int MOSTLoraWAN::getDutyCycle()
 
 /////////////////////////////////////////
 // PLCheck: 0 - disable, 1 - enable
-boolean MOSTLoraWAN::setPLCheck(int state)
+boolean WANsetPLCheck(int state)
 {
     sprintf(_strBuf, "AAT2 PLCheck=%d", state);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-int MOSTLoraWAN::getPLCheck()
+int WANgetPLCheck()
 {
     char *strRet = command(F("AAT2 PLCheck=?"));
     return atoi(strRet);
@@ -292,14 +292,14 @@ int MOSTLoraWAN::getPLCheck()
 
 /////////////////////////////////////////
 // Rx2_Freq_DR: frequency 000000001~999999999 in Hz, data-rate 0~15
-boolean MOSTLoraWAN::setRx2_Freq_DR(long freq, int dataRate)
+boolean WANsetRx2_Freq_DR(long freq, int dataRate)
 {
     sprintf(_strBuf, "AAT2 Rx2_Freq_DR=%ld,%d", freq, dataRate);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-void MOSTLoraWAN::getRx2_Freq_DR(long &freq, int &dataRate)
+void WANgetRx2_Freq_DR(long &freq, int &dataRate)
 {
     char *strRet = command(F("AAT2 Rx2_Freq_DR=?"));
     char *token;
@@ -311,23 +311,21 @@ void MOSTLoraWAN::getRx2_Freq_DR(long &freq, int &dataRate)
     if (token != NULL) {
         dataRate = atoi(token + 2);
     }
-#ifdef DEBUG_LORA
-    debugSerial.print(freq);
-    debugSerial.print(F("Hz, dataRate:"));
-    debugSerial.println(dataRate);
-#endif // DEBUG_LORA
+#ifdef USE_DEBUG_OUTPUT
+    printDebug("%dHz, dataRate:%d\n", freq, dataRate);
+#endif // USE_DEBUG_OUTPUT
 }
 
 /////////////////////////////////////////
 // ClassMode: 0 - Class A, 2 - Class C
-boolean MOSTLoraWAN::setClassMode(int mode)
+boolean WANsetClassMode(int mode)
 {
     sprintf(_strBuf, "AAT2 ClassMode=%d", mode);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-int MOSTLoraWAN::getClassMode()
+int WANgetClassMode()
 {
     char *strRet = command(F("AAT2 ClassMode=?"));
     return atoi(strRet);
@@ -335,14 +333,14 @@ int MOSTLoraWAN::getClassMode()
 
 /////////////////////////////////////////
 // Rx1DrOffset: index of Rx1 offset 0~3
-boolean MOSTLoraWAN::setRx1DrOffset(int offset)
+boolean WANsetRx1DrOffset(int offset)
 {
     sprintf(_strBuf, "AAT2 Rx1DrOffset=%d", offset);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-int MOSTLoraWAN::getRx1DrOffset()
+int WANgetRx1DrOffset()
 {
     char *strRet = command(F("AAT2 Rx1DrOffset=?"));
     return atoi(strRet);
@@ -355,14 +353,14 @@ int MOSTLoraWAN::getRx1DrOffset()
 //      data-rate 0~15
 //      channel state: 0 - close, 1 - open
 //      band grouping: US 0, EU 0~3
-boolean MOSTLoraWAN::setTx_Channel(int channel, long freq, int dataRate, int channelOpen, int bandGroup)
+boolean WANsetTx_Channel(int channel, long freq, int dataRate, int channelOpen, int bandGroup)
 {
     sprintf(_strBuf, "AAT2 Tx_Channel=%d,%ld,%d,%d,%d", channel, freq, dataRate, channelOpen, bandGroup);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-void MOSTLoraWAN::getTx_Channel(int channel, long &freq, int &dataRate, int &channelOpen, int &bandGroup)
+void WANgetTx_Channel(int channel, long &freq, int &dataRate, int &channelOpen, int &bandGroup)
 {
     sprintf(_strBuf, "AAT2 Tx_Channel%d=?", channel);
     char *strRet = command(_strBuf);
@@ -383,30 +381,21 @@ void MOSTLoraWAN::getTx_Channel(int channel, long &freq, int &dataRate, int &cha
     if (token != NULL) {
         bandGroup = atoi(token + 4);
     }
-#ifdef DEBUG_LORA
-    debugSerial.print(F("Ch_"));
-    debugSerial.print(channel);
-    debugSerial.print(F(", Freq:"));
-    debugSerial.print(freq);
-    debugSerial.print(F("Hz, dataRate:"));
-    debugSerial.print(dataRate);
-    debugSerial.print(F(", status:"));
-    debugSerial.print(channelOpen);
-    debugSerial.print(F(", band:"));
-    debugSerial.println(bandGroup);
-#endif // DEBUG_LORA
+#ifdef USE_DEBUG_OUTPUT
+    printDebug("Ch_%d, Freq:%dHz, dataRate:%d, status:%d, band:%d\n", channel, freq, dataRate, channelOpen, bandGroup);
+#endif // USE_DEBUG_OUTPUT
 }
 
 /////////////////////////////////////////
 // Tx_Band[x]: band grouping: US 0, EU 0~3, dutyCycle 1~9999, ID-power 0~15
-boolean MOSTLoraWAN::setTx_Band(int bandGroup, int dutyCycle, int idPower)
+boolean WANsetTx_Band(int bandGroup, int dutyCycle, int idPower)
 {
     sprintf(_strBuf, "AAT2 Tx_Band=%d,%d,%d", bandGroup, dutyCycle, idPower);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-void MOSTLoraWAN::getTx_Band(int bandGroup, int &dutyCycle, int &idPower)
+void WANgetTx_Band(int bandGroup, int &dutyCycle, int &idPower)
 {
     if (bandGroup >= 0) {
         sprintf(_strBuf, "AAT2 Tx_Band%d=?", bandGroup);
@@ -424,20 +413,16 @@ void MOSTLoraWAN::getTx_Band(int bandGroup, int &dutyCycle, int &idPower)
     if (token != NULL) {
         idPower = atoi(token + 8);
     }
-#ifdef DEBUG_LORA
-    debugSerial.print(F("band:"));
-    debugSerial.print(bandGroup);
-    debugSerial.print(F(", dutyCycle:"));
-    debugSerial.print(dutyCycle);
-    debugSerial.print(F(", TxPower ID:"));
-    debugSerial.println(idPower);
+#ifdef USE_DEBUG_OUTPUT
+    printDebug("band:%d, dutyCycle:%d, TxPower ID:%d\n", bandGroup, dutyCycle, idPower);
+    , channelOpen, bandGroup);
 
-#endif // DEBUG_LORA */
+#endif // USE_DEBUG_OUTPUT
 }
 
 /////////////////////////////////////////
 // Uplink_Count
-int MOSTLoraWAN::getUplink_Count()
+int WANgetUplink_Count()
 {
     char *strRet = command(F("AAT2 Uplink_Count=?"));
     return atoi(strRet);
@@ -445,7 +430,7 @@ int MOSTLoraWAN::getUplink_Count()
 
 /////////////////////////////////////////
 // Downlink_Count
-int MOSTLoraWAN::getDownlink_Count()
+int WANgetDownlink_Count()
 {
     char *strRet = command(F("AAT2 Downlink_Count=?"));
     return atoi(strRet);
@@ -453,14 +438,14 @@ int MOSTLoraWAN::getDownlink_Count()
 
 /////////////////////////////////////////
 // Tx_Power[x]: ID-power 0~15, dbm: US 0~30, EU 0~20
-boolean MOSTLoraWAN::setTx_Power(int idPower, int dbmPower)
+boolean WANsetTx_Power(int idPower, int dbmPower)
 {
     sprintf(_strBuf, "AAT2 Tx_Power=%d,%d", idPower, dbmPower);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-int MOSTLoraWAN::getTx_Power(int idPower)
+int WANgetTx_Power(int idPower)
 {
     if (idPower >= 0) {
         sprintf(_strBuf, "AAT2 Tx_Power%d=?", idPower);
@@ -480,14 +465,14 @@ int MOSTLoraWAN::getTx_Power(int idPower)
 
 /////////////////////////////////////////
 // Pl_Max_Length[x]: dataRate 0~15, payload length: 0~255
-boolean MOSTLoraWAN::setPl_Max_Length(int dataRate, int lenPayload)
+boolean WANsetPl_Max_Length(int dataRate, int lenPayload)
 {
     sprintf(_strBuf, "AAT2 Pl_Max_Length=%d,%d", dataRate, lenPayload);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-int MOSTLoraWAN::getPl_Max_Length(int dataRate)
+int WANgetPl_Max_Length(int dataRate)
 {
     if (dataRate >= 0) {
         sprintf(_strBuf, "AAT2 Pl_Max_Length%d=?", dataRate);
@@ -507,14 +492,14 @@ int MOSTLoraWAN::getPl_Max_Length(int dataRate)
 
 /////////////////////////////////////////
 // Plre_Max_Length[x]: dataRate 0~15, payload length: 0~255
-boolean MOSTLoraWAN::setPlre_Max_Length(int dataRate, int lenPayload)
+boolean WANsetPlre_Max_Length(int dataRate, int lenPayload)
 {
     sprintf(_strBuf, "AAT2 Plre_Max_Length=%d,%d", dataRate, lenPayload);
     char *strRet = command(_strBuf);
     
     return isOK(strRet);
 }
-int MOSTLoraWAN::getPlre_Max_Length(int dataRate)
+int WANgetPlre_Max_Length(int dataRate)
 {
     if (dataRate >= 0) {
         sprintf(_strBuf, "AAT2 Plre_Max_Length%d=?", dataRate);
--- lora_wan.h
+++ lora_wan.h
@@ -7,5 +7,106 @@ extern void initLoraWAN();
 
 extern bool isOK(const char *strResponse);
 
-extern const char *getFwVersion();
 
+extern void WANupdateFW();
+extern void WANsaveSetting();
+extern const char *WANgetFwVersion();
+extern void WANreset();
+extern void WANsetSLEEP();
+extern void WANrestore();
+
+// send payload by Tx: port: 1~223, cnf/uncnf, Hex-payload(11 bytes)
+extern boolean WANsetTx(int port, char *confirm, char *payload);
+
+// TestMode: 0 - disable, 1 - enable
+extern boolean WANsetTestMode(int mode);
+extern int WANgetTestMode();
+
+extern boolean WANsetDevAddr(const char *strAddr);
+extern const char *WANgetDevAddr();
+
+extern boolean WANsetDevEui(const char *strEui);
+extern const char *WANgetDevEui();
+
+extern boolean setAppEui(const char *strAddr);
+extern const char *WANgetAppEui();
+
+extern boolean setNwkSKey(const char *strKey);
+extern const char *WANgetNwkSKey();
+
+extern boolean setAppSKey(const char *strKey);
+extern const char *WANgetAppSKey();
+
+extern boolean setAppKey(const char *strKey);
+extern const char *WANgetAppKey();
+
+// ADR: 0 - disable, 1 - enable
+extern boolean WANsetADR(int state);
+extern int WANgetADR();
+
+// EVK_TxCycle: 1~254 sec
+extern boolean WANsetEVK_TxCycle(int interval);
+extern int WANgetEVK_TxCycle();
+
+// JoinMode: 0 - disable, 1 - enable
+extern boolean WANsetJoinMode(int mode);
+extern int WANgetJoinMode();
+
+// reTx: 0~8
+extern boolean WANsetreTx(int retry);
+extern int WANgetreTx();
+
+// RxDelay1: 100000~10000000
+extern boolean WANsetRxDelay1(long delayMicroSec);
+extern long WANgetRxDelay1();
+
+// DutyCycle: 0 - disable, 1 - enable
+extern boolean WANsetDutyCycle(int state);
+extern int WANgetDutyCycle();
+
+// PLCheck: 0 - disable, 1 - enable
+extern boolean WANsetPLCheck(int state);
+extern int WANgetPLCheck();
+
+// Rx2_Freq_DR: frequency 000000001~999999999 in Hz, data-rate 0~15
+extern boolean WANsetRx2_Freq_DR(long freq, int dataRate);
+extern void WANgetRx2_Freq_DR(long &freq, int &dataRate);
+
+// ClassMode: 0 - Class A, 2 - Class C
+extern boolean WANsetClassMode(int mode);
+extern int WANgetClassMode();
+
+// Rx1DrOffset: index of Rx1 offset 0~3
+extern boolean WANsetRx1DrOffset(int offset);
+extern int WANgetRx1DrOffset();
+
+// Tx_Channel[x]:
+//      channel: US 0~71, EU 0~15
+//      frequency 000000001~999999999 in Hz
+//      data-rate 0~15
+//      channel state: 0 - close, 1 - open
+//      band grouping: US 0, EU 0~3
+extern boolean WANsetTx_Channel(int channel, long freq, int dataRate, int channelOpen, int bandGroup);
+extern void WANgetTx_Channel(int channel, long &freq, int &dataRate, int &channelOpen, int &bandGroup);
+
+// Tx_Band[x]: band grouping: US 0, EU 0~3, dutyCycle 1~9999, ID-power 0~15
+extern boolean WANsetTx_Band(int bandGroup, int dutyCycle, int idPower);
+extern void WANgetTx_Band(int bandGroup, int &dutyCycle, int &idPower);
+
+// Uplink_Count
+extern int WANgetUplink_Count();
+
+// Downlink_Count
+extern int WANgetDownlink_Count();
+
+// Tx_Power[x]: ID-power 0~15, dbm: US 0~30, EU 0~20
+extern boolean WANsetTx_Power(int idPower, int dbmPower);
+extern int WANgetTx_Power(int idPower);
+
+// Pl_Max_Length[x]: dataRate 0~15, payload length: 0~255
+extern boolean WANsetPl_Max_Length(int dataRate, int lenPayload);
+extern int WANgetPl_Max_Length(int dataRate);
+
+// Plre_Max_Length[x]: dataRate 0~15, payload length: 0~255
+extern boolean WANsetPlre_Max_Length(int dataRate, int lenPayload);
+extern int WANgetPlre_Max_Length(int dataRate);
